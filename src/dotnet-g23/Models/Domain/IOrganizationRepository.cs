454c670eb947680e9fd2711fbdb8452941055eb7
PrebenLeroy <preben.leroy@live.be>
2017-02-18 11:42:52 +0100

    Adding interface IGebruikersRepository

--- src/dotnet-g23/Models/Domain/IOrganizationRepository.cs
+++ src/dotnet-g23/Models/Domain/IOrganizationRepository.cs
@@ -7,6 +7,9 @@ namespace dotnet_g23.Models.Domain
 {
     public interface IOrganizationRepository
     {
-
+        Organization GetBy(String naam);
+        Organization GetByLocation(String locatie);
+        IEnumerable<Organization> GetAll();
+        void SaveChanges();
     }
 }
