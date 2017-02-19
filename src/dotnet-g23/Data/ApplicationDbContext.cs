9e45694a7e18d2f971d0cb787423e6d0d2ab3527
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-02-19 17:11:06 +0100

    Add ApplicationDbContext

---
+++ src/dotnet-g23/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_g23.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
