2c74d97c14f2aefec7966c92cef9d91b13d35210
PrebenLeroy <preben.leroy@live.be>
2017-02-19 12:07:18 +0100

    Start implementing GADOrganization

--- src/dotnet-g23/Models/Domain/GADOrganization.cs
+++ src/dotnet-g23/Models/Domain/GADOrganization.cs
@@ -1,11 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace dotnet_g23.Models.Domain
 {
     public class GADOrganization
     {
+        private String _naam;
+        private String _locatie;
+        private IOrganizationRole _organizationRole;
+
+        public IOrganizationRole OrganizationRole
+        {
+            get { return _organizationRole; }
+            set { _organizationRole = value; }
+        }
+
+        public String Naam { get { return _naam; } private set { _naam = value; } }
+
+        public String Locatie { get { return _locatie; } private set { _locatie = value; } }
+
+        public GADOrganization(String naam, String locatie, IOrganizationRole organizationRole)
+        {
+            Naam = naam;
+            Locatie = locatie;
+            SetOrganizationRole(organizationRole);
+        }
+
+        private void SetOrganizationRole(IOrganizationRole organizationRole)
+        {
+            OrganizationRole = organizationRole;
+        }
     }
 }
