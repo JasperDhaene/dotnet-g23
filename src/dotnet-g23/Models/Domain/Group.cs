914a5b73da5f3dfa0dfd4910a5564351996f6889
PrebenLeroy <preben.leroy@live.be>
2017-02-20 20:04:26 +0100

    Start implementing Group

--- src/dotnet-g23/Models/Domain/Group.cs
+++ src/dotnet-g23/Models/Domain/Group.cs
@@ -7,5 +7,27 @@ namespace dotnet_g23.Models.Domain
 {
     public class Group
     {
+        private String _naam;
+        private Boolean _isGesloten = true;
+
+        public String Naam{
+            get { return _naam; }
+            private set {
+                if(value.Equals(null) || value.Trim().Equals(null))
+                {
+                    throw new ArgumentException("Naam mag niet leeg zijn!");
+                }
+                _naam = value;
+            } }
+
+        public Group(String naam)
+        {
+            Naam = naam;
+        }
+
+        public Group(String naam, Boolean isGesloten) : base(naam)
+        {
+            _isGesloten = isGesloten;
+        }
     }
 }
