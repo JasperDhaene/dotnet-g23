008eee4a9889ea57ad24b72129d7a73d311b0784
Tuur Lievens <tuur.lievens@hotmail.com>
2017-02-21 21:56:28 +0100

    Start ManageController

--- src/dotnet-g23/Controllers/ManageController.cs
+++ src/dotnet-g23/Controllers/ManageController.cs
@@ -2,18 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnet_g23.Models.Domain;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace dotnet_g23.Controllers
 {
-    public class ManageController : Controller
-    {
-        // GET: /<controller>/
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class ManageController : Controller
+	{
+		// GET: /manage/
+		public IActionResult Index()
+		{
+			// show list of open groups
+			return View("Index");
+		}
+
+		public IActionResult Register(string name, Participant user) {
+			// find group, check if user isn't registered and register user with group
+
+			// redirect to group
+			return View();
+		}
+
+		public IActionResult Create(string name, int type, Participant user)
+		{
+			// validate name, make new group and register user
+
+			// check if groupname unique and not empty
+
+			//GBOrganization org = GBOrganization.RegisterGroup(name);
+			//org.RegisterUser(user);
+
+			//notify lector
+			return View("Create");
+		}
+
+
+		public IActionResult Invite(string[] addresses)
+		{
+			foreach (string address in addresses)
+			{
+				//validate mail address
+				//invite mail address
+			}
+
+			//notify lector
+			return View();
+		}
+
+	}
 }
