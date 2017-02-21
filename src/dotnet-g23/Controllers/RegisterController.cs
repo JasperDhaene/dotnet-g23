cfed89cfa729465554868ceb06d84f5b52547b58
Tuur Lievens <tuur.lievens@hotmail.com>
2017-02-21 21:56:36 +0100

    Start RegisterController

--- src/dotnet-g23/Controllers/RegisterController.cs
+++ src/dotnet-g23/Controllers/RegisterController.cs
@@ -2,18 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnet_g23.Models.Domain;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+namespace dotnet_g23.Controllers {
+	public class RegisterController : Controller {
+		// GET: /register/
+		public IActionResult Index(string query = null) {
+			// return filtered list with name & location of GB organisations
+			GBOrganization[] list = OrganizationRepository.getAll();
+			if (query != null)
+				list = list.Where(o => (o.Name.IndexOf(query) > -1 | o.Location.IndexOf(query) > -1));
+			return View("Index");
+		}
 
-namespace dotnet_g23.Controllers
-{
-    public class RegisterController : Controller
-    {
-        // GET: /<controller>/
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+		public IActionResult Register(GBOrganization org, GADUser user) {
+
+			string[] ApprovedMailExtensions = { "temp.com" };
+
+			// validate & register user or throw error
+			if (Array.IndexOf(ApprovedMailExtensions, GetMailExtension(user.Email)) > -1) {
+				//org.RegisterUser(user);
+				// redirect to group
+				return View();
+			}
+			else {
+				//show error
+				return View();
+			}
+		}
+
+		private string GetMailExtension(String address) {
+			return address.Substring(address.IndexOf('@'), address.Length - 1);
+		}
+
+	}
 }
