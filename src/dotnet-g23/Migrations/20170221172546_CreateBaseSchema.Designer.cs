f3c6c300ca463e359a963a06f73659a10107a8e3
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-02-21 18:28:47 +0100

    Add base schema migration

---
+++ src/dotnet-g23/Migrations/20170221172546_CreateBaseSchema.Designer.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using dotnet_g23.Data;
+
+namespace dotnetg23.Migrations
+{
+    [DbContext(typeof(ApplicationDbContext))]
+    [Migration("20170221172546_CreateBaseSchema")]
+    partial class CreateBaseSchema
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("dotnet_g23.Models.Domain.GADOrganization", b =>
+                {
+                    b.Property<int>("OrganizationId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Location");
+
+                    b.Property<string>("Name");
+
+                    b.Property<int?>("OrganizationRoleId");
+
+                    b.HasKey("OrganizationId");
+
+                    b.HasIndex("OrganizationRoleId");
+
+                    b.ToTable("GADOrganization");
+                });
+
+            modelBuilder.Entity("dotnet_g23.Models.Domain.GADUser", b =>
+                {
+                    b.Property<int>("UserId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Email");
+
+                    b.Property<int?>("UserRoleId");
+
+                    b.HasKey("UserId");
+
+                    b.HasIndex("UserRoleId");
+
+                    b.ToTable("GADUser");
+                });
+
+            modelBuilder.Entity("dotnet_g23.Models.Domain.Group", b =>
+                {
+                    b.Property<int>("GroupId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int?>("GBOrganizationOrganizationRoleId");
+
+                    b.Property<int?>("LectorUserRoleId");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("GroupId");
+
+                    b.HasIndex("GBOrganizationOrganizationRoleId");
+
+                    b.HasIndex("LectorUserRoleId");
+
+                    b.ToTable("Groups");
+                });
+
+            modelBuilder.Entity("dotnet_g23.Models.Domain.OrganizationRole", b =>
+                {
+                    b.Property<int>("OrganizationRoleId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("organization_role_type")
+                        .IsRequired();
+
+                    b.HasKey("OrganizationRoleId");
+
+                    b.ToTable("OrganizationRoles");
+
+                    b.HasDiscriminator<string>("organization_role_type").HasValue("OrganizationRole");
+                });
+
+            modelBuilder.Entity("dotnet_g23.Models.Domain.UserRole", b =>
+                {
+                    b.Property<int>("UserRoleId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("user_role_type")
+                        .IsRequired();
+
+                    b.HasKey("UserRoleId");
+
+                    b.ToTable("UserRoles");
+
+                    b.HasDiscriminator<string>("user_role_type").HasValue("UserRole");
+                });
+
+            modelBuilder.Entity("dotnet_g23.Models.Domain.GBOrganization", b =>
+                {
+                    b.HasBaseType("dotnet_g23.Models.Domain.OrganizationRole");
+
+
+                    b.ToTable("GBOrganizations");
+
+                    b.HasDiscriminator().HasValue("organization_role_gb_organization");
+                });
+
+            modelBuilder.Entity("dotnet_g23.Models.Domain.Organization", b =>
+                {
+                    b.HasBaseType("dotnet_g23.Models.Domain.OrganizationRole");
+
+
+                    b.ToTable("Organizations");
+
+                    b.HasDiscriminator().HasValue("organization_role_organization");
+                });
+
+            modelBuilder.Entity("dotnet_g23.Models.Domain.Lector", b =>
+                {
+                    b.HasBaseType("dotnet_g23.Models.Domain.UserRole");
+
+
+                    b.ToTable("Lectors");
+
+                    b.HasDiscriminator().HasValue("user_role_lector");
+                });
+
+            modelBuilder.Entity("dotnet_g23.Models.Domain.Participant", b =>
+                {
+                    b.HasBaseType("dotnet_g23.Models.Domain.UserRole");
+
+                    b.Property<int?>("GroupId");
+
+                    b.HasIndex("GroupId");
+
+                    b.ToTable("Participants");
+
+                    b.HasDiscriminator().HasValue("user_role_participant");
+                });
+
+            modelBuilder.Entity("dotnet_g23.Models.Domain.Volunteer", b =>
+                {
+                    b.HasBaseType("dotnet_g23.Models.Domain.UserRole");
+
+
+                    b.ToTable("Volunteers");
+
+                    b.HasDiscriminator().HasValue("user_role_volunteer");
+                });
+
+            modelBuilder.Entity("dotnet_g23.Models.Domain.GADOrganization", b =>
+                {
+                    b.HasOne("dotnet_g23.Models.Domain.OrganizationRole", "OrganizationRole")
+                        .WithMany()
+                        .HasForeignKey("OrganizationRoleId");
+                });
+
+            modelBuilder.Entity("dotnet_g23.Models.Domain.GADUser", b =>
+                {
+                    b.HasOne("dotnet_g23.Models.Domain.UserRole", "UserRole")
+                        .WithMany()
+                        .HasForeignKey("UserRoleId");
+                });
+
+            modelBuilder.Entity("dotnet_g23.Models.Domain.Group", b =>
+                {
+                    b.HasOne("dotnet_g23.Models.Domain.GBOrganization", "GBOrganization")
+                        .WithMany("Groups")
+                        .HasForeignKey("GBOrganizationOrganizationRoleId");
+
+                    b.HasOne("dotnet_g23.Models.Domain.Lector", "Lector")
+                        .WithMany("Groups")
+                        .HasForeignKey("LectorUserRoleId");
+                });
+
+            modelBuilder.Entity("dotnet_g23.Models.Domain.Participant", b =>
+                {
+                    b.HasOne("dotnet_g23.Models.Domain.Group", "Group")
+                        .WithMany("Participants")
+                        .HasForeignKey("GroupId");
+                });
+        }
+    }
+}
---
+++ src/dotnet-g23/Migrations/20170221172546_CreateBaseSchema.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace dotnetg23.Migrations
+{
+    public partial class CreateBaseSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrganizationRoles",
+                columns: table => new
+                {
+                    OrganizationRoleId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    organization_role_type = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrganizationRoles", x => x.OrganizationRoleId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GADOrganization",
+                columns: table => new
+                {
+                    OrganizationId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Location = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    OrganizationRoleId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GADOrganization", x => x.OrganizationId);
+                    table.ForeignKey(
+                        name: "FK_GADOrganization_OrganizationRoles_OrganizationRoleId",
+                        column: x => x.OrganizationRoleId,
+                        principalTable: "OrganizationRoles",
+                        principalColumn: "OrganizationRoleId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserRoles",
+                columns: table => new
+                {
+                    UserRoleId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    user_role_type = table.Column<string>(nullable: false),
+                    GroupId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRoles", x => x.UserRoleId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GADUser",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Email = table.Column<string>(nullable: true),
+                    UserRoleId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GADUser", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_GADUser_UserRoles_UserRoleId",
+                        column: x => x.UserRoleId,
+                        principalTable: "UserRoles",
+                        principalColumn: "UserRoleId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    GroupId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    GBOrganizationOrganizationRoleId = table.Column<int>(nullable: true),
+                    LectorUserRoleId = table.Column<int>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.GroupId);
+                    table.ForeignKey(
+                        name: "FK_Groups_OrganizationRoles_GBOrganizationOrganizationRoleId",
+                        column: x => x.GBOrganizationOrganizationRoleId,
+                        principalTable: "OrganizationRoles",
+                        principalColumn: "OrganizationRoleId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Groups_UserRoles_LectorUserRoleId",
+                        column: x => x.LectorUserRoleId,
+                        principalTable: "UserRoles",
+                        principalColumn: "UserRoleId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GADOrganization_OrganizationRoleId",
+                table: "GADOrganization",
+                column: "OrganizationRoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GADUser_UserRoleId",
+                table: "GADUser",
+                column: "UserRoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_GBOrganizationOrganizationRoleId",
+                table: "Groups",
+                column: "GBOrganizationOrganizationRoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_LectorUserRoleId",
+                table: "Groups",
+                column: "LectorUserRoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRoles_GroupId",
+                table: "UserRoles",
+                column: "GroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_Groups_GroupId",
+                table: "UserRoles",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "GroupId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Groups_OrganizationRoles_GBOrganizationOrganizationRoleId",
+                table: "Groups");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Groups_UserRoles_LectorUserRoleId",
+                table: "Groups");
+
+            migrationBuilder.DropTable(
+                name: "GADOrganization");
+
+            migrationBuilder.DropTable(
+                name: "GADUser");
+
+            migrationBuilder.DropTable(
+                name: "OrganizationRoles");
+
+            migrationBuilder.DropTable(
+                name: "UserRoles");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+        }
+    }
+}
