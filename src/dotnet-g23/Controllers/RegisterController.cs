1f114af312fb9bb989b4d2518d1758523c22b28e
Tuur Lievens <tuur.lievens@hotmail.com>
2017-02-22 16:42:27 +0100

    Now compilable!

--- src/dotnet-g23/Controllers/RegisterController.cs
+++ src/dotnet-g23/Controllers/RegisterController.cs
@@ -21,7 +21,7 @@ namespace dotnet_g23.Controllers {
 		[Route("organizations")]
 		public IActionResult Index(string query = null) {
 			// return filtered list with name & location of GB organisations
-			GADOrganization[] list = _orgRepository.GetAll().Where(o => o.OrganizationRole.ToString() == "GBOrganization" );
+			IEnumerable<GADOrganization> list = _orgRepository.GetAll().Where(o => o.OrganizationRole.ToString() == "GBOrganization" );
 			if (query != null)
 				list = list.Where(o => (o.Name.IndexOf(query) > -1 | o.Location.IndexOf(query) > -1));
 			return View("Index");
