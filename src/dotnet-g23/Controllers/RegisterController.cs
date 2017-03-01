c81228878a4f82b14fbab17304dad8feb04e12df
Tuur Lievens <tuur.lievens@hotmail.com>
2017-03-01 16:20:21 +0100

    Redo RegisterController

--- src/dotnet-g23/Controllers/RegisterController.cs
+++ src/dotnet-g23/Controllers/RegisterController.cs
@@ -5,53 +5,57 @@ using System.Threading.Tasks;
 using dotnet_g23.Data.Repositories;
 using dotnet_g23.Helpers;
 using dotnet_g23.Models.Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_g23.Controllers {
+	[Authorize]
 	public class RegisterController : Controller {
 
 		#region Fields
-		//private User _user;
 		private readonly IUserRepository _userRepository;
 		private readonly IOrganizationRepository _orgRepository;
+		private readonly UserManager<ApplicationUser> _userManager;
 		#endregion
 
 		#region Constructors
-		public RegisterController(UserRepository userRepository, OrganizationRepository orgRepository) {
+		public RegisterController(IUserRepository userRepository, IOrganizationRepository orgRepository, UserManager<ApplicationUser> userManager) {
 			_userRepository = userRepository;
 			_orgRepository = orgRepository;
-
-			//_user = _userRepository.GetAll().First();
+			_userManager = userManager;
 		}
 		#endregion
 
 		#region Methods
-		//[Route("organizations")]
+		[Route("organizations")]
 		public IActionResult Index(string query = null) {
 			// return filtered list with name & location of GB organisations
-			/*IEnumerable<Organization> list = _orgRepository.GetAll().Where(o => o.OrganizationRole.ToString() == "Organization" );
+
+			IEnumerable<Organization> list = _orgRepository.GetAll();
 			if (query != null)
-				list = list.Where(o => (o.Name.IndexOf(query) > -1 || o.Location.IndexOf(query) > -1));*/
+				list = list.Where(o => (o.Name.IndexOf(query) > -1 || o.Location.IndexOf(query) > -1));
+
+			ViewData["organizations"] = list;
 			return View();
 		}
 
-		//[Route("organizations/{orgname}")]
+		[HttpPost]
 		public IActionResult Register(string orgname) {
 			// validate & register user with org or throw error
-            /*
-			Organization org = _orgRepository.GetByName(orgname);
 
-			if (MailHelper.VerifyMailAddress(_user.Email))
-			{
-				//org.Participants.Add(_user);
-				return RedirectToAction("Index", "ManageController");
+			Organization org = _orgRepository.GetByName(orgname);
+			var user = _userManager.FindByNameAsync(User.Identity.Name);
+
+			if (MailHelper.VerifyMailAddress(user.Result.Email)) {
+				org.Participants.Add(new Participant(org));
+				return RedirectToAction("Index", "GroupManageController");
 			}
 			else {
 				ViewData["Message"] = "Error: Wrong mail address";
 				return View();
 			}
-            */
-		    return View();
+
 		}
 		#endregion
 
