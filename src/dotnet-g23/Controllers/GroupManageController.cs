256f604e7e37aa04f5f8f4a110c369c5a1f1b079
Tuur Lievens <tuur.lievens@hotmail.com>
2017-03-01 16:20:10 +0100

    Redo GroupManageController

--- src/dotnet-g23/Controllers/GroupManageController.cs
+++ src/dotnet-g23/Controllers/GroupManageController.cs
@@ -5,70 +5,77 @@ using System.Threading.Tasks;
 using dotnet_g23.Data.Repositories;
 using dotnet_g23.Helpers;
 using dotnet_g23.Models.Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_g23.Controllers
 {
+	[Authorize]
 	public class GroupManageController : Controller
 	{
 
 		#region Fields
-		//private User _user;
 		private readonly IUserRepository _userRepository;
 		private readonly IOrganizationRepository _orgRepository;
+		private readonly UserManager<ApplicationUser> _userManager;
 		#endregion
 
 		#region Constructors
-		public GroupManageController(IUserRepository userRepository, IOrganizationRepository orgRepository) {
+		public GroupManageController(IUserRepository userRepository, IOrganizationRepository orgRepository, UserManager<ApplicationUser> userManager) {
 			_userRepository = userRepository;
 			_orgRepository = orgRepository;
-
-			//_user = _userRepository.GetAll().First();
+			_userManager = userManager;
 		}
 		#endregion
 
 		#region Methods
-		//[Route("groups")]
+		[Route("groups")]
 		public IActionResult Index()
 		{
 
-            // show list of open groups
-            /*IEnumerable<Organization> orgList = _orgRepository.GetAll().Where(o => o.OrganizationRole.ToString() == "Organization").Cast<Organization>();
-
-			Group[] list = {};
+			// show list of open groups
+			IEnumerable<Organization> orgList = _orgRepository.GetAll();
+			IEnumerable<Group> list = new List<Group>();
 
 			foreach (Organization org in orgList)
 				foreach (Group group in org.Groups)
-					if (!group.IsClosed())
-						list[list.Length-1] = group;
+					list.Append(group);
 
-			return View(list);
-            */
-            return View();
+			ViewData["groups"] = list;
+			return View();
 
+		}
 
-        }
-
-		//[HttpPost]
-		//[Route("groups/register")]
+		[HttpPost]
 		public IActionResult Register(string name) {
 			// find group, check if user isn't registered and register user with group
 
-			//Group group = _groupRepo.GetByName(name);
-			//group.Participants.Add(_user);
+			Group group;
 
-			//if (_user.Group != null)
-			//{
-				//ViewData["Message"] = "Error: already registered";
-				//return View();
-			//}
+			IEnumerable<Organization> orgList = _orgRepository.GetAll();
+			foreach (Organization org in orgList)
+				foreach (Group gr in org.Groups)
+					if (gr.Name == name)
+						group = gr;
+
+			var authuser = _userManager.FindByNameAsync(User.Identity.Name);
+			GUser user = _userRepository.GetByEmail(authuser.Result.Email);
+
+			/*if (user.Group != null)
+			{
+				group.Participants.Add(user);
+				user.Group = group;
+				ViewData["Message"] = "Error: already registered";
+				return View();
+			}*/
 
 
 			// redirect to group detail
-			return RedirectToAction("Index", "Manage");
+			return RedirectToAction("Index", "GroupManageController");
 		}
 
-		//[HttpPost]
+		[HttpPost]
 		//[Route("groups/create")]
 		public IActionResult Create(string name = null, bool closed = false)
 		{
@@ -81,16 +88,20 @@ namespace dotnet_g23.Controllers
 			}
 
 			// check if groupname unique and not empty
-			//Organization org = _user.Group.Organization;
-			//Group group = org.CreateGroup(name);
-			//group.Participants.Add(_user);
+			var authuser = _userManager.FindByNameAsync(User.Identity.Name);
+			GUser user = _userRepository.GetByEmail(authuser.Result.Email);
+
+			/*Organization org = user.Organization;
+			Group group = new Group(name);
+			group.Participants.Add(user);
+			org.Groups.Add(group);*/
 
 			// notify lector
 			//_user.Lector.Notify();
 			return View();
 		}
 
-		//[HttpPost]
+		[HttpPost]
 		//[Route("groups/invite")]
 		public IActionResult Invite(string[] addresses = null)
 		{
