22173dc59e9f63cde94a7195e6e57130c0fd41f9
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-03-01 18:12:24 +0100

    Add GroupRepository

---
+++ src/dotnet-g23/Models/Domain/IGroupRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_g23.Models.Domain
+{
+    public interface IGroupRepository
+    {
+        Group GetBy(int groupId);
+        Group GetByName(String groupName);
+        IEnumerable<Group> GetAll();
+        void SaveChanges();
+    }
+}
