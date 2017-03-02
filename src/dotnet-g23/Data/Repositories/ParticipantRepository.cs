87023a90f7f3c135ae22042f1f0a93de91f7557c
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-03-02 15:56:59 +0100

    Add ParticipantRepository

---
+++ src/dotnet-g23/Data/Repositories/ParticipantRepository.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using dotnet_g23.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_g23.Data.Repositories
+{
+    public class ParticipantRepository : IParticipantRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<Participant> _participants;
+
+        public ParticipantRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _participants = context.Participants;
+        }
+
+        public IEnumerable<Participant> GetAll()
+        {
+            return _participants
+                .Include(p => p.Group)
+                .Include(p => p.Lector)
+                .Include(p => p.Organization)
+                .Include(p => p.User)
+                .ToList();
+        }
+
+
+
+        public Participant GetBy(int userStateId)
+        {
+            return _participants
+                .Include(p => p.Group)
+                .Include(p => p.Lector)
+                .Include(p => p.Organization)
+                .Include(p => p.User)
+                .SingleOrDefault(p => p.UserRoleId == userStateId);
+        }
+
+        public Participant GetByUser(int userId)
+        {
+            return _participants
+                .Include(p => p.Group)
+                .Include(p => p.Lector)
+                .Include(p => p.Organization)
+                .Include(p => p.User)
+                .SingleOrDefault(p => p.User.UserId == userId);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ src/dotnet-g23/Models/Domain/IParticipantRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_g23.Models.Domain
+{
+    public interface IParticipantRepository
+    {
+        Participant GetBy(int userStateId);
+        Participant GetByUser(int userId);
+        IEnumerable<Participant> GetAll();
+        void SaveChanges();
+    }
+}
