6c88ada5c2c42a5c93330522bf4de8dbeecad689
PrebenLeroy <preben.leroy@live.be>
2017-03-02 11:08:23 +0100

    Started to write Filter classes

---
+++ src/dotnet-g23/Filters/UserFilter.cs
@@ -0,0 +1,17 @@
+﻿using dotnet_g23.Models.Domain;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Beerhall.Filters {
+    public class CustomerFilter : ActionFilterAttribute {
+        private readonly IUserRepository _userRepository;
+
+        public CustomerFilter(IUserRepository userRespoitory) {
+            _userRepository = userRespoitory;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context) {
+            context.ActionArguments["guser"] = context.HttpContext.User.Identity.IsAuthenticated ? _userRepository.GetByEmail(context.HttpContext.User.Identity.Name) : null;
+            base.OnActionExecuting(context);
+        }
+    }
+}
