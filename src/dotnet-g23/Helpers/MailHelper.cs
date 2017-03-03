46c0d5d7464509e407e143be7caea7a495d228b0
Tuur Lievens <tuur.lievens@hotmail.com>
2017-03-03 16:14:03 +0100

    Remove VerifyMailAddress method in MailHelper

--- src/dotnet-g23/Helpers/MailHelper.cs
+++ src/dotnet-g23/Helpers/MailHelper.cs
@@ -7,14 +7,6 @@ namespace dotnet_g23.Helpers
 {
     public class MailHelper
     {
-
-	    public static bool VerifyMailAddress(string address, string orgdomain)
-	    {
-		    if (orgdomain == GetMailDomain(address))
-				return true;
-		    return false;
-	    }
-
 	    public static string GetMailDomain(string address) {
 			return address.Substring(address.IndexOf('@'), address.Length - 1);
 		}
