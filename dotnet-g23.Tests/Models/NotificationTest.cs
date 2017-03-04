0e970b83e52b130cb182c28c95e1709dd50b34a9
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-03-04 15:34:12 +0100

    Add Notification class

---
+++ dotnet-g23.Tests/Models/NotificationTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_g23.Models.Domain;
+using Xunit;
+
+namespace dotnet_g23.Tests.Models
+{
+    public class NotificationTest
+    {
+        [Fact]
+        public void NewNotificationIsUnread()
+        {
+            Notification notification = new Notification(null, null, null);
+            Assert.False(notification.IsRead);
+        }
+
+        [Fact]
+        public void ReadNotificationIsRead()
+        {
+            Notification notification = new Notification(null, null, null);
+            notification.Read();
+            Assert.True(notification.IsRead);
+        }
+    }
+}
---
+++ src/dotnet-g23/Models/Domain/Notification.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Server.Kestrel.Internal.Http;
+
+namespace dotnet_g23.Models.Domain
+{
+    public class Notification
+    {
+        #region Properties
+        public GUser User { get; private set; }
+        public Group Group { get; private set; }
+        public String Message { get; private set; }
+        public DateTime DateCreated { get; private set; }
+        public DateTime DateRead { get; private set; }
+        public Boolean IsRead { get; private set; }
+        #endregion
+
+        #region Constructors
+        public Notification(Group fromGroup, GUser toUser, String message)
+        {
+            Group = fromGroup;
+            User = toUser;
+            Message = message;
+            DateCreated = DateTime.Now;
+            IsRead = false;
+        }
+        #endregion
+
+        #region Methods
+
+        public void Read()
+        {
+            DateRead = DateTime.Now;
+            IsRead = true;
+        }
+        #endregion
+    }
+}
