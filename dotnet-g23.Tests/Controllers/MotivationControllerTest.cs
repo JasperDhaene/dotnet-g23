5f3ad2f8b1e94d68c2c893a91453339e0f42a314
PrebenLeroy <preben.leroy@live.be>
2017-03-05 15:12:56 +0100

    Add MotivationControllerTest

---
+++ dotnet-g23.Tests/Controllers/MotivationControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_g23.Tests.Controllers
+{
+    public class MotivationControllerTest
+    {
+        [Fact(Skip = "Not yet implemented")]
+        public void Test1() {
+
+        }
+        [Fact(Skip = "Not yet implemented")]
+        public void Test2() {
+
+        }
+        [Fact(Skip = "Not yet implemented")]
+        public void Test3() {
+
+        }
+    }
+}
