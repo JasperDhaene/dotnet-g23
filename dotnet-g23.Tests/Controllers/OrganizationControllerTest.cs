97a86b8e6538c3e54966e7ca7f56aaeed2acec41
PrebenLeroy <preben.leroy@live.be>
2017-03-05 15:46:15 +0100

    Start implementation org test

--- dotnet-g23.Tests/Controllers/OrganizationControllerTest.cs
+++ dotnet-g23.Tests/Controllers/OrganizationControllerTest.cs
@@ -1,26 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Xunit;
+﻿using dotnet_g23.Controllers;
+using dotnet_g23.Models.Domain;
+using dotnet_g23.Models.Domain.Repositories;
+using dotnet_g23.Models.ViewModels.OrganizationViewModels;
+using Moq;
 
-namespace dotnet_g23.Tests.Controllers
-{
+namespace dotnet_g23.Tests.Controllers {
     public class OrganizationControllerTest
     {
-        [Fact (Skip = "Not yet implemented")]
-        public void Test1()
-        {
+        private OrganizationController _controller;
+        private GUser _user;
+        private Mock<IOrganizationRepository> _mockOrgRepo;
+        private IndexViewModel _model;
+        private IndexViewModel _modelMetFout;
 
-        }
-        [Fact(Skip = "Not yet implemented")]
-        public void Test2()
-        {
-
-        }
-        [Fact(Skip = "Not yet implemented")]
-        public void Test3()
-        {
+        public OrganizationControllerTest() {
 
         }
     }
