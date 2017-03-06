75c2d439a252f0ce5773cd39d87bef84c54a1d5e
PrebenLeroy <preben.leroy@live.be>
2017-03-06 19:53:53 +0100

    Start implement MotivationController

--- src/dotnet-g23/Controllers/MotivationController.cs
+++ src/dotnet-g23/Controllers/MotivationController.cs
@@ -4,17 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using dotnet_g23.Filters;
+using dotnet_g23.Models.Domain.Repositories;
+using dotnet_g23.Models.Domain;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace dotnet_g23.Controllers
-{
-    
-    public class MotivationController : Controller
-    {
-        // GET: /<controller>/
-        public IActionResult Index()
-        {
+namespace dotnet_g23.Controllers {
+    [Authorize]
+    [ServiceFilter(typeof(UserFilter))]
+    public class MotivationController : Controller {
+        #region Fields
+        private readonly IGroupRepository _groupRepository;
+        #endregion
+
+        public MotivationController(IGroupRepository groupRepository) {
+            _groupRepository = groupRepository;
+        }
+
+        public IActionResult Index(Organization organization) {
+            IEnumerable<Group> groups = _groupRepository.GetByOrganization(organization);
+            IEnumerable<Motivation> motivationsToWrite;
             return View();
         }
 
@@ -23,8 +33,8 @@ namespace dotnet_g23.Controllers
             return View();
         }
 
-        [Authorize(Policy ="lector")]
-        public IActionResult CheckMotivation() {
+        [Authorize(Policy = "lector")]
+        public IActionResult CheckMotivation(GUser user) {
             return View();
         }
     }
