6280653a31d05d0944fee94549a4ecee379f00e1
PrebenLeroy <preben.leroy@live.be>
2017-03-06 09:00:18 +0100

    Add scripts

---
+++ src/dotnet-g23/wwwroot/js/CustomScripts/FilterScript.js
@@ -0,0 +1,15 @@
+﻿//var filterView = {
+
+//    init: function () {
+//        $("form").submit(function () {
+//            $.post(this.action, $(this).serialize(), function (data) {
+//                $("#NameLocation").html(data);
+//            });
+//            return false;
+//        });
+//    }
+//}
+
+//$(function () {
+//    filterView.init();
+//});
