35256878d49ab76584bbcd41ca40d69a18a47460
PrebenLeroy <preben.leroy@live.be>
2017-03-12 10:02:51 +0100

    Update viewmodels

--- src/dotnet-g23/Models/ViewModels/GroupViewModels/IndexViewModel.cs
+++ src/dotnet-g23/Models/ViewModels/GroupViewModels/IndexViewModel.cs
@@ -4,12 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using dotnet_g23.Models.Domain;
 
-namespace dotnet_g23.Models.ViewModels.GroupViewModels
-{
-    public class IndexViewModel
-    {
+namespace dotnet_g23.Models.ViewModels.GroupViewModels {
+    public class IndexViewModel {
         public IEnumerable<Group> InvitedGroups { get; set; }
         public IEnumerable<Group> OpenGroups { get; set; }
         public Group SubscribedGroup { get; set; }
+        public Organization Organization { get; set; }
     }
 }
