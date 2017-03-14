3bbbd8d88b7e55cfb2ada958e54d964f85d75466
PrebenLeroy <preben.leroy@live.be>
2017-03-14 20:35:20 +0100

    Add default dropdownlist

--- src/dotnet-g23/Models/ViewModels/MotivationViewModels/IndexViewModel.cs
+++ src/dotnet-g23/Models/ViewModels/MotivationViewModels/IndexViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using dotnet_g23.Models.Domain;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +12,16 @@ namespace dotnet_g23.Models.ViewModels.MotivationViewModels
         public Group Group { get; set; }
 
         public Motivation Motivation { get; set; }
+
+        public SelectList PossibleCompaniesList { get; set; }
+
+        public IndexViewModel() {
+            int[] maanden = new int[] { 1, 2, 3, 6, 12, 18, 24 };
+            List<SelectListItem> items = new List<SelectListItem>();
+            foreach (int maand in maanden)
+                items.Add(new SelectListItem() { Text = maand + " " + ((maand == 1) ? "maand" : "maanden"), Value = maand.ToString() });
+            PossibleCompaniesList = new SelectList(items, "Value", "Text");
+        }
+
     }
 }
