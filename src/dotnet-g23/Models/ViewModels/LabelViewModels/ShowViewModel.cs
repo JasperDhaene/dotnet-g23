c72c0f438895d09c13066bf0df72b8e120db6960
PrebenLeroy <preben.leroy@live.be>
2017-03-14 22:40:28 +0100

    Add ViewModel

---
+++ src/dotnet-g23/Models/ViewModels/LabelViewModels/ShowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_g23.Models.ViewModels.LabelViewModels
+{
+    public class ShowViewModel
+    {
+        public Compary Company { get; set; }
+
+        public IEnumerable<Contact> Contacts { get; set; }
+    }
+}
