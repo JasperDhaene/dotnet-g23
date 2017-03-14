dbe8862bb8e9137e49e3ba57d40d97aa745418c0
PrebenLeroy <preben.leroy@live.be>
2017-03-14 20:50:27 +0100

    Add Indexmethod to controller

--- src/dotnet-g23/Controllers/LabelController.cs
+++ src/dotnet-g23/Controllers/LabelController.cs
@@ -3,17 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using dotnet_g23.Filters;
+using Microsoft.AspNetCore.Authorization;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace dotnet_g23.Controllers
 {
+    [Authorize]
+    [ServiceFilter(typeof(ParticipantFilter))]
     public class LabelController : Controller
     {
+        #region fields
+        private readonly ICompanyRepository _companyRepository;
+        #endregion
+
+        #region Constructor
+        public LabelController(ICompanyRepository compRepo) {
+            _companyRepository = compRepo;
+        }
+        #endregion
+
+
+
         // GET: /<controller>/
-        public IActionResult Index()
+        public IActionResult Index(GUser user, Participant participant, String query = null)
         {
-            return View();
+            IndexViewModel vm = new IndexViewModel() {
+                ChosenCompany = participant?.Organization,
+                Companies = query == null ? _companyRepository.GetAll() : _companyRepository.GetByKeyword(query)
+            };
+            return View(vm);
         }
     }
 }
