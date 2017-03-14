036786bfeab04cab50924227bd8f87135c80851f
PrebenLeroy <preben.leroy@live.be>
2017-03-14 20:59:46 +0100

    Make CompanyRepository (with build errors because no Company class)

---
+++ src/dotnet-g23/Data/Repositories/CompanyRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_g23.Data.Repositories
+{
+    public class CompanyRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<Company> _companies;
+
+        public CompanyRepository(ApplicationDbContext context) {
+            _context = context;
+            _companies = context.Companies;
+        }
+
+        public IEnumerable<Company> GetAll() {
+            return _companies
+                .ToList();
+        }
+
+        public Company GetBy(int companyId) {
+            return _companies
+                .SingleOrDefault(c => c.CompanyId == companyId);
+        }
+
+        public IEnumerable<Company> GetByKeyword(String query) {
+            return _companies
+                .Where(org => org.Name.Contains(query) || org.Location.Contains(query))
+                .ToList();
+        }
+
+        public Company GetByName(String orgName) {
+            return _companies
+                .SingleOrDefault(o => o.Name == orgName);
+        }
+
+        public void SaveChanges() {
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ src/dotnet-g23/Models/Domain/Repositories/ICompanyRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_g23.Models.Domain.Repositories
+{
+    public interface ICompanyRepository
+    {
+        Company GetBy(int companyId);
+        Company GetByName(String compName);
+        IEnumerable<Company> GetAll();
+        void SaveChanges();
+        IEnumerable<Company> GetByKeyword(String query);
+    }
+}
