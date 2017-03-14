104186308a28d2bc06ef3c4c9dc4b3cf582a7868
PrebenLeroy <preben.leroy@live.be>
2017-03-14 22:01:20 +0100

    Add IndexViewModel for Label

---
+++ src/dotnet-g23/Models/ViewModels/LabelViewModels/IndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using dotnet_g23.Models.Domain;
+
+namespace dotnet_g23.Models.ViewModels.LabelViewModels {
+    public class IndexViewModel
+    {
+        public IEnumerable<Company> Companies { get; set; }
+        public Company ChosenCompany { get; set; }
+    }
+}
