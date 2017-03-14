d063974537b2e6105e5dcad04f30e174e58fa452
PrebenLeroy <preben.leroy@live.be>
2017-03-14 13:35:58 +0100

    update tekst for mailservice

--- src/dotnet-g23/Services/AuthMessageSender.cs
+++ src/dotnet-g23/Services/AuthMessageSender.cs
@@ -6,10 +6,8 @@ namespace dotnet_g23.Services {
     // This class is used by the application to send Email and SMS
     // when you turn on two-factor authentication in ASP.NET Identity.
     // For more details see this link http://go.microsoft.com/fwlink/?LinkID=532713
-    public class AuthMessageSender
-    {
-        public void SendEmailAsync(string receiver, string email, string subject, string message)
-        {
+    public class AuthMessageSender {
+        public void SendEmailAsync(string organizationName, string receiver, string beschrijving, string email, string subject, string message) {
             // Plug in your email service here to send an email.
             var emailMessage = new MimeMessage();
             emailMessage.From.Add(new MailboxAddress("Goed Bezig!", "goedbezig@dejonckhee.re"));
@@ -17,7 +15,16 @@ namespace dotnet_g23.Services {
             emailMessage.Subject = subject;
 
             emailMessage.Body = new TextPart(TextFormat.Text) {
-                Text = message
+                Text = "Geachte \n" +
+                "Omdat het deugd doet om een compliment te krijgen en omdat iedereen een extra hart onder de riem best kan gebruiken, " +
+                "nemen wij, cursisten van "+ organizationName +", deel aan het initiatief ‘Goed bezig!’.\n\n" +
+                "Via het initiatief ‘Goed bezig!’ geven we een label als erkenning aan een organisatie waarvan wij vinden dat deze goed bezig is.\n\n" +
+                "Wij hebben jullie, " + receiver + " , gekozen omdat we het enorm waarderen dat jullie " + beschrijving + ". "+
+                "Jullie doen dit vrijwillig en zetten jullie elke dag belangeloos in!Jullie zijn ‘Goed bezig!’\n\n"+
+                "Jullie krijgen niet alleen het label, we steken ook graag de handen uit de mouwen om jullie te ondersteunen.We hebben reeds enkele ideeën rond mogelijke acties, "+
+                "en hadden deze graag aan jullie voorgesteld.\n\n"+
+                "Het digitale label kunnen jullie hier alvast bekijken en delen via onze facebookpagina: www.facebook.com/jebentgoedbezig \n\n"+
+                "Met vriendelijke groet"
             };
 
             using (var client = new SmtpClient()) {
