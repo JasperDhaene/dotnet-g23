92714cb9498d2bdfbadc7a1b202850f2e4d23ddc
PrebenLeroy <preben.leroy@live.be>
2017-03-15 18:22:50 +0100

    Add Action class + methods

---
+++ src/dotnet-g23/Models/Domain/Action.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_g23.Models.Domain
+{
+    public class Action
+    {
+        #region Attribute
+        private String _description;
+        #endregion
+
+        #region Properties
+        public String Titel { get; set; }
+        public String Description {
+            get {
+                return _description;
+            }
+            set {
+                if (value.Equals(null) || value.Equals("") || value.Trim(' ').Equals(""))
+                    throw new ArgumentException("Omschrijving mag niet leeg zijn!");
+                _description = value;
+            }
+        } 
+        #endregion
+
+        #region Constructors
+        public Action() {
+
+        }
+
+        public Action(string titel, string description) {
+            Titel = titel;
+            Description = description;
+        } 
+        #endregion
+    }
+}
