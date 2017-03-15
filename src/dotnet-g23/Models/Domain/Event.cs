9e4563cece12e5ac909ae5832e6382ea55aa8565
PrebenLeroy <preben.leroy@live.be>
2017-03-15 18:34:43 +0100

    Add Event class with constructor

---
+++ src/dotnet-g23/Models/Domain/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_g23.Models.Domain
+{
+    public class Event : Action
+    {
+        public DateTime Date { get;  set; }
+
+        #region Constructor
+        public Event() : base() { }
+
+        public Event(string titel, string description, DateTime date) : base(titel, description) {
+            Date = date;
+        } 
+        #endregion
+    }
+}
