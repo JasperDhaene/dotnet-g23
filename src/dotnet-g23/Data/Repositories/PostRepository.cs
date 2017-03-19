fd35ac60b7db2471f36c4c7050500f10bf0582ef
PrebenLeroy <preben.leroy@live.be>
2017-03-19 15:24:36 +0100

    Start implementation PostRepository, not yet in ApplicationDbContext

---
+++ src/dotnet-g23/Data/Repositories/PostRepository.cs
@@ -0,0 +1,41 @@
+﻿using dotnet_g23.Models.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_g23.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_g23.Data.Repositories
+{
+    public class PostRepository : IPostRepository {
+
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<Post> _posts;
+
+        public PostRepository(ApplicationDbContext context) {
+            _context = context;
+            _posts = _context.Posts();
+        }
+
+        public IEnumerable<Post> GetAll() {
+            throw new NotImplementedException();
+        }
+
+        public Post GetBy(int postId) {
+            throw new NotImplementedException();
+        }
+
+        public Post GetByGroup(Group group) {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetByOrganization(Organization organization) {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges() {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/dotnet-g23/Models/Domain/Repositories/IPostRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_g23.Models.Domain.Repositories
+{
+    public interface IPostRepository
+    {
+        Post GetBy(int postId);
+        Post GetByGroup(Group group);
+        IEnumerable<Post> GetAll();
+        IEnumerable<Post> GetByOrganization(Organization organization);
+        void SaveChanges();
+    }
+}
