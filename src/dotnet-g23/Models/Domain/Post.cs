7588a9ebeb46d4764b106b7fa0168693a383ce7e
PrebenLeroy <preben.leroy@live.be>
2017-03-19 12:22:54 +0100

    Delete unnecessary usings

--- src/dotnet-g23/Models/Domain/Post.cs
+++ src/dotnet-g23/Models/Domain/Post.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Drawing;
-using System.IO;
 
 namespace dotnet_g23.Models.Domain {
     public class Post {
