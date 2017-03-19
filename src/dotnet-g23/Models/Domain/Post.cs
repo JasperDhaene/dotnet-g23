622f9f1cc73212ea90af6f9753d086f13c50c84e
PrebenLeroy <preben.leroy@live.be>
2017-03-19 12:49:56 +0100

    Add properties to class to link with other classes

--- src/dotnet-g23/Models/Domain/Post.cs
+++ src/dotnet-g23/Models/Domain/Post.cs
@@ -9,6 +9,10 @@ namespace dotnet_g23.Models.Domain {
         public String Announcement { get; set; }
         public Byte[] Logo { get; set; }
         public Motivation Motivation { get; set; }
+        public Group Group { get; set; }
+        public int GroupForeignKey { get; set; }
+        public Organization Organization { get; set; }
+        public int OrganizationForeignKey { get; set; }
         #endregion
 
         #region Constructors
