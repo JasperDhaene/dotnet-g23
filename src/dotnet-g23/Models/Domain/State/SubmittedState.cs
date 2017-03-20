7d360af677b6e20019b1e671000a3c955a1d2644
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-03-20 18:40:35 +0100

    Fix InitialState inheritance bug

--- src/dotnet-g23/Models/Domain/State/SubmittedState.cs
+++ src/dotnet-g23/Models/Domain/State/SubmittedState.cs
@@ -5,8 +5,20 @@ using System.Threading.Tasks;
 
 namespace dotnet_g23.Models.Domain.State
 {
-    public class SubmittedState : InitialState
+    public class SubmittedState : State
     {
+        public override void Invite(Context context, Group group, Participant participant)
+        {
+            // DRY
+            new InitialState().Invite(context, group, participant);
+        }
+
+        public override void Register(Context context, Group group, Participant participant)
+        {
+            // DRY
+            new InitialState().Register(context, group, participant);
+        }
+
         public override void Submit(Context context, Group group)
         {
             throw new StateException("Motivatie is reeds verzonden");
