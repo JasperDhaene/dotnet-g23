71bbc4bbf045e0a28cd2bbb51afa41ce265df720
PrebenLeroy <preben.leroy@live.be>
2017-03-22 20:44:16 +0100

    Add 2 test methods

---
+++ dotnet-g23.Tests/Models/ContactTest.cs
@@ -0,0 +1,27 @@
+﻿using dotnet_g23.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace dotnet_g23.Tests.Models {
+    public class ContactTest {
+        [Fact]
+        public void ConstructorShouldCreateNewContact() {
+            String title = "Mr.";
+            String fN = "Foo";
+            String lN = "Bar";
+            String func = "CEO";
+            String email = "foo.bar@gar.com";
+            Company comp = new Company("Foobar", "Foobar", "Foobar", "foobar.com", "foobar@foobar.com");
+            Contact contact = new Contact(title, fN, lN, func, email, comp);
+
+            Assert.Equal(comp, contact.Company);
+            Assert.Equal(title, contact.Title);
+            Assert.Equal(fN, contact.FirstName);
+            Assert.Equal(lN, contact.LastName);
+            Assert.Equal(email, contact.Email);
+        }
+    }
+}
---
+++ dotnet-g23.Tests/Models/PostTest.cs
@@ -0,0 +1,22 @@
+﻿using dotnet_g23.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace dotnet_g23.Tests.Models
+{
+    public class PostTest
+    {
+        [Fact]
+        public void ConstructorShouldCreateNewPost() {
+            String announcement = "Foobar";
+            byte[] logo = new byte[] { 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 };
+
+            Post post = new Post(announcement, logo);
+            Assert.Equal(announcement, post.Announcement);
+            Assert.Equal(logo, post.Logo);
+        }
+    }
+}
