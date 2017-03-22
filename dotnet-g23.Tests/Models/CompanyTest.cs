ff6fd181ff34d3cb8c3d85bdc09f55d4ff117fa7
PrebenLeroy <preben.leroy@live.be>
2017-03-22 20:30:30 +0100

    Add company test of constructor

---
+++ dotnet-g23.Tests/Models/CompanyTest.cs
@@ -0,0 +1,24 @@
+﻿using dotnet_g23.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace dotnet_g23.Tests.Models
+{
+    public class CompanyTest
+    {
+        [Fact]
+        public void ConstructorShouldCreateNewCompany() {
+            String name = "foo";
+            String descr = "foobar";
+            String address = "foo bar";
+            String website = "foo.bar";
+            String email = "foo@bar";
+
+            Company company = new Company(name, descr, address, website, email);
+            Assert.True(company.Contacts.Count() == 0);
+        }
+    }
+}
