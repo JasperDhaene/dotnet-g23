c1bd33fafef82e8b180e9bddcdb8fb20c8868c3e
PrebenLeroy <preben.leroy@live.be>
2017-03-22 20:15:55 +0100

    Add invitationTest

---
+++ dotnet-g23.Tests/Models/InvitationTest.cs
@@ -0,0 +1,20 @@
+﻿using dotnet_g23.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace dotnet_g23.Tests.Models
+{
+    public class InvitationTest
+    {
+        [Fact]
+        public void ConstructorShouldCreateNewInvitation() {
+            Group group = new Group("Open Group");
+            GUser user = new GUser("john.doe@foo.bar", new Participant(new Organization("Foo", "Bar", "foo.bar")));
+            Invitation inv = new Invitation(group, user.UserState as Participant);
+            Assert.Equal(group.Name, inv.Group.Name);
+        }
+    }
+}
