6471b7ddba2a827482e8688b740eaefbb2ae1a1b
PrebenLeroy <preben.leroy@live.be>
2017-03-22 19:08:14 +0100

    Rewrite constructor of test

--- dotnet-g23.Tests/Controllers/OrganizationControllerTest.cs
+++ dotnet-g23.Tests/Controllers/OrganizationControllerTest.cs
@@ -1,102 +1,63 @@
-﻿//using dotnet_g23.Controllers;
-//using dotnet_g23.Models.Domain;
-//using dotnet_g23.Models.Domain.Repositories;
-//using dotnet_g23.Models.ViewModels.OrganizationViewModels;
-//using dotnet_g23.Tests.Data;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.ViewFeatures;
-//using Moq;
-//using System.Collections;
-//using System.Collections.Generic;
-//using System.Linq;
-//using Xunit;
+﻿using dotnet_g23.Controllers;
+using dotnet_g23.Models.Domain;
+using dotnet_g23.Models.Domain.Repositories;
+using dotnet_g23.Models.ViewModels.OrganizationViewModels;
+using dotnet_g23.Tests.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Moq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
 
-//namespace dotnet_g23.Tests.Controllers {
-//    public class OrganizationControllerTest {
-//        #region Attributes
-//        private readonly OrganizationController _controller;
-//        private readonly GUser _user1;
-//        private readonly GUser _user2;
-//        private DummyApplicationDbContext context;
-//        #endregion
+namespace dotnet_g23.Tests.Controllers {
+    public class OrganizationControllerTest {
+        #region Attributes
+        private readonly OrganizationController _controller;
+        private DummyApplicationDbContext _context;
+        private readonly GUser _user1;
+        private readonly GUser _user2;
+        
+        #endregion
 
-//        #region Constructor
-//        public OrganizationControllerTest() {
-//            context = new DummyApplicationDbContext();
+        #region Constructor
+        public OrganizationControllerTest() {
+            _context = new DummyApplicationDbContext();
 
-//            Mock<IOrganizationRepository> repo = new Mock<IOrganizationRepository>();
-//            Mock<IGroupRepository> Grepo = new Mock<IGroupRepository>();
-//            Mock<IPostRepository> Prepo = new Mock<IPostRepository>();
+            Mock<UserManager<ApplicationUser>> userMan = new Mock<UserManager<ApplicationUser>>();
+            Mock<SignInManager<ApplicationUser>> signMan = new Mock<SignInManager<ApplicationUser>>();
+            Mock<IOrganizationRepository> repo = new Mock<IOrganizationRepository>();
+            Mock<IGroupRepository> Grepo = new Mock<IGroupRepository>();
+            Mock<IPostRepository> Prepo = new Mock<IPostRepository>();
 
-//            repo.Setup(o => o.GetAll()).Returns(context.Organizations);
+            repo.Setup(o => o.GetAll()).Returns(_context.Organizations);
 
-//            repo.Setup(o => o.GetBy(1)).Returns(context.org1);
-//            repo.Setup(o => o.GetBy(2)).Returns(context.org2);
-//            repo.Setup(o => o.GetBy(3)).Returns(context.org3);
+            repo.Setup(o => o.GetBy(1)).Returns(_context.HogentGent);
+            repo.Setup(o => o.GetBy(2)).Returns(_context.HogentAalst);
+            repo.Setup(o => o.GetBy(3)).Returns(_context.HowestKortrijk);
+            repo.Setup(o => o.GetBy(4)).Returns(_context.HowestBrugge);
+            repo.Setup(o => o.GetBy(5)).Returns(_context.Ugent);
 
-//            repo.Setup(o => o.GetByDomain("hogent.be")).Returns(context.OrgsHogent);
-//            repo.Setup(o => o.GetByDomain("howest.be")).Returns(context.OrgsHowest);
-//            repo.Setup(o => o.GetByDomain("organization.be"))
-//                .Returns(context.OrgsOrganization);
+            Grepo.Setup(o => o.GetAll()).Returns(_context.Groups);
 
-//            _controller = new OrganizationController(repo.Object, Grepo.Object, Prepo.Object);
-//            _controller.TempData = new Mock<ITempDataDictionary>().Object;
+            Grepo.Setup(o => o.GetBy(1)).Returns(_context.HogentGroup);
+            Grepo.Setup(o => o.GetBy(2)).Returns(_context.HogentGroupSubmitted);
+            Grepo.Setup(o => o.GetBy(3)).Returns(_context.HogentGroupApproved);
+            Grepo.Setup(o => o.GetBy(4)).Returns(_context.HogentGroupGranted);
+            Grepo.Setup(o => o.GetBy(5)).Returns(_context.HogentGroupAnnounced);
 
-//            _user1 = context.Preben;
-//            _user2 = context.Tuur;
+            _controller = new OrganizationController(userMan.Object, signMan.Object, repo.Object, Grepo.Object, Prepo.Object);
+            _controller.TempData = new Mock<ITempDataDictionary>().Object;
 
-//        }
-//        #endregion
+            
 
-//        #region Index
+        }
+        #endregion
 
-//        [Fact]
-//        public void IndexGivenListOfPossibleOrganizationsWithUser1() {
-//            ViewResult result = _controller.Index(_user1, _user1.UserState as Participant, null) as ViewResult;
-//            IndexViewModel ind1 = (IndexViewModel)result?.Model;
-//            IEnumerable<Organization> orgResult = ind1.Organizations;
-//            Assert.False(orgResult?.Count() == 0);
-//        }
-
-//        [Fact]
-//        public void IndexGivenListOfPossibleOrganizationsWithUser2() {
-//            ViewResult result = _controller.Index(_user2, _user2.UserState as Participant, null) as ViewResult;
-//            IndexViewModel ind2 = (IndexViewModel)result?.Model;
-//            IEnumerable<Organization> orgResult = ind2.Organizations;
-//            Assert.False(orgResult?.Count() == 0);
-//        }
-
-//        [Fact]
-//        public void IndexShouldGiveEmptySubscribedOrganizationForUser1() {
-//            ViewResult result = _controller.Index(_user1, _user1.UserState as Participant, null) as ViewResult;
-//            IndexViewModel ind1 = (IndexViewModel)result?.Model;
-//            Assert.Equal(null, ind1.SubscribedOrganization);
-//        }
-
-//        [Fact]
-//        public void IndexShouldGiveSubscribedOrganizationForUser2() {
-//            ViewResult result = _controller.Index(_user2, _user2.UserState as Participant, null) as ViewResult;
-//            IndexViewModel ind2 = (IndexViewModel)result?.Model;
-//            Assert.Equal(context.org3, ind2.SubscribedOrganization);
-//        }
-
-//        #endregion
-
-//        #region Register Post
-
-//        [Fact]
-//        public void RegisterShouldRedirectToActionForUser1RegisterToOrganizationAfterRegistration() {
-//            RedirectToActionResult result = _controller.Register(_user1, context.org1.OrganizationId) as RedirectToActionResult;
-//            Assert.Equal("Index", result?.ActionName);
-//            Assert.Equal("Organizations", result?.ControllerName);
-//        }
-
-//        [Fact]
-//        public void RegisterShouldRedirectToActionForUser2RegisterToOrganizationWhenAlreadyInOrganization() {
-//            RedirectToActionResult result = _controller.Register(_user2, context.org3.OrganizationId) as RedirectToActionResult;
-//            Assert.Equal("Index", result.ActionName);
-//            Assert.Equal("Organizations", result.ControllerName);
-//        }
-//        #endregion
-//    }
-//}
+        #region Index
+        
+        #endregion
+    }
+}
