a8de5b3853caa57d0060461855a28e3f3d7ec67c
PrebenLeroy <preben.leroy@live.be>
2017-03-22 20:25:48 +0100

    Add labeltest of constructor

---
+++ dotnet-g23.Tests/Models/LabelTest.cs
@@ -0,0 +1,22 @@
+﻿using dotnet_g23.Models;
+using dotnet_g23.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace dotnet_g23.Tests.Models
+{
+    public class LabelTest
+    {
+        [Fact]
+        public void ContstructorShouldCreateLabel() {
+            Group group = new Group("OpenGroup");
+            Company comp = new Company("foobar", "foobar", "foobar", "foobar.com", "foo@bar.com");
+            Label label = new Label(group, comp);
+            Assert.Equal(group, label.Group);
+            Assert.Equal(comp, label.Company);
+        }
+    }
+}
