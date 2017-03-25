76fca556aa9d83dd93082fcc5deb0b1b043b2979
PrebenLeroy <preben.leroy@live.be>
2017-03-25 11:13:21 +0100

    Start writing MotivationControllerTest

--- dotnet-g23.Tests/Controllers/MotivationControllerTest.cs
+++ dotnet-g23.Tests/Controllers/MotivationControllerTest.cs
@@ -1,65 +1,60 @@
-﻿//using dotnet_g23.Controllers;
-//using dotnet_g23.Models.Domain;
-//using dotnet_g23.Models.Domain.Repositories;
-//using dotnet_g23.Models.ViewModels.MotivationViewModels;
-//using dotnet_g23.Tests.Data;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.ViewFeatures;
-//using Moq;
-//using System.Collections;
-//using System.Collections.Generic;
-//using System.Linq;
-//using Xunit;
+﻿using dotnet_g23.Controllers;
+using dotnet_g23.Models.Domain;
+using dotnet_g23.Models.Domain.Repositories;
+using dotnet_g23.Models.ViewModels.MotivationViewModels;
+using dotnet_g23.Tests.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Moq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
 
-//namespace dotnet_g23.Tests.Controllers {
-//    public class MotivationControllerTest {
-//        #region Attributes
-//        private readonly MotivationController _controller;
-//        private readonly Participant _user1;
-//        private readonly Participant _user2;
-//        private DummyApplicationDbContext context;
-//        #endregion
+namespace dotnet_g23.Tests.Controllers {
+    public class MotivationControllerTest {
+        #region Attributes
+        private readonly MotivationController _controller;
+        private DummyApplicationDbContext context;
+        private readonly Participant _ownerHogent;
+        private readonly Participant _ownerHogentSubmitted;
+        #endregion
 
-//        #region Constructor
-//        public MotivationControllerTest() {
-//            context = new DummyApplicationDbContext();
+        #region Constructor
+        public MotivationControllerTest() {
+            context = new DummyApplicationDbContext();
 
-//            Mock<IGroupRepository> GroupRepo = new Mock<IGroupRepository>();
+            Mock<IGroupRepository> GroupRepo = new Mock<IGroupRepository>();
 
-//            GroupRepo.Setup(g => g.GetAll()).Returns(context.Groups);
-//            GroupRepo.Setup(g => g.GetBy(0)).Returns(context.Groups.First());
-//            GroupRepo.Setup(o => o.GetBy(1)).Returns(context.Groups.Skip(1).First());
-//            GroupRepo.Setup(g => g.GetByName("OpenGroup")).Returns(context.Groups.First());
+            GroupRepo.Setup(g => g.GetAll()).Returns(context.Groups);
+            GroupRepo.Setup(g => g.GetBy(1)).Returns(context.HogentGroup);
+            GroupRepo.Setup(o => o.GetBy(2)).Returns(context.HogentGroupSubmitted);
+            GroupRepo.Setup(g => g.GetBy(3)).Returns(context.HogentGroupApproved);
+            GroupRepo.Setup(o => o.GetBy(4)).Returns(context.HogentGroupGranted);
+            GroupRepo.Setup(g => g.GetBy(5)).Returns(context.HogentGroupAnnounced);
+            GroupRepo.Setup(g => g.GetByName("HogentGroup")).Returns(context.Groups.First());
 
-//            _controller = new MotivationController(GroupRepo.Object);
-//            _controller.TempData = new Mock<ITempDataDictionary>().Object;
+            _controller = new MotivationController(GroupRepo.Object);
+            _controller.TempData = new Mock<ITempDataDictionary>().Object;
 
-//            _user1 = context.Preben2.UserState as Participant;
-//            _user2 = context.Tuur.UserState as Participant;
-//        }
-//        #endregion
+            _ownerHogent = context.OwnerHogent.UserState as Participant;
+            _ownerHogentSubmitted = context.OwnerHogentSubmitted.UserState as Participant;
+        }
+        #endregion
 
-//        #region Show
+        #region Edit - GET
+        [Fact]
+        public void ParticipantWhereGroupHasNoMotivationCanCreateNewMotivationBecauseIsNull() {
+            ViewResult result = _controller.Edit(_ownerHogent, _ownerHogent.Group.GroupId) as ViewResult;
+            ShowViewModel vm = (ShowViewModel)result?.Model;
+            Assert.Equal(_ownerHogent.Group, vm.Group);
+            Assert.Null(vm.Motivation);
+        }
 
-//        [Fact]
-//        public void ShowShouldShowGroupOfParticipant() {
-//            ViewResult result = _controller.Show(_user2, 1) as ViewResult;
-//            IndexViewModel vm = (IndexViewModel)result?.Model;
-//            Assert.NotNull(vm?.Group);
-//        }
+        //[Fact]
+        //public void ParticipantWhereGroupHasMotivationCanShowMotivation() {
 
-//        #endregion
-
-//        #region Update
-
-//        [Fact]
-//        public void UpdateShouldShowDetailsOfGroupWithUpdatedMotivation() {
-//            RedirectToActionResult result = _controller
-//                .Update(_user1, 1, context.Motivation) as RedirectToActionResult;
-//            Assert.Equal("Show", result?.ActionName);
-//            Assert.Equal("Group", result?.ControllerName);
-//        }
-
-//        #endregion
-//    }
-//}
+        //}
+        #endregion
+    }
+}
