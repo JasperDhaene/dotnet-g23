ba6f7a866bd2727d7cb2a7c3f6a7930daf126bf6
PrebenLeroy <preben.leroy@live.be>
2017-03-25 17:26:09 +0100

    Start writing ActionController attributes

--- dotnet-g23.Tests/Controllers/ActionControllerTest.cs
+++ dotnet-g23.Tests/Controllers/ActionControllerTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using dotnet_g23.Controllers;
+using dotnet_g23.Models.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,8 @@ namespace dotnet_g23.Tests.Controllers
 {
     public class ActionControllerTest
     {
+        private readonly ActionController _controller;
+        private Participant _OwnerGranted;
+        private Participant _OwnerAnnounced;
     }
 }
