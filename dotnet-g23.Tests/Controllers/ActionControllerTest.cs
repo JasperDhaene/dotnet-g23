eed4bab53886a8150a9c24b5bd0d60bbfd9bee3c
PrebenLeroy <preben.leroy@live.be>
2017-03-26 15:09:52 +0200

    Add method (not working yet)

--- dotnet-g23.Tests/Controllers/ActionControllerTest.cs
+++ dotnet-g23.Tests/Controllers/ActionControllerTest.cs
@@ -1,12 +1,16 @@
 ﻿using dotnet_g23.Controllers;
 using dotnet_g23.Models.Domain;
 using dotnet_g23.Models.Domain.Repositories;
+using dotnet_g23.Models.ViewModels.ActionViewModels;
 using dotnet_g23.Tests.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace dotnet_g23.Tests.Controllers
 {
@@ -24,7 +28,32 @@ namespace dotnet_g23.Tests.Controllers
             context = new DummyApplicationDbContext();
 
             Mock<IParticipantRepository> PartRepo = new Mock<IParticipantRepository>();
+            Mock<IGroupRepository> GroupRepo = new Mock<IGroupRepository>();
+
+            GroupRepo.Setup(g => g.GetAll()).Returns(context.Groups);
+            GroupRepo.Setup(g => g.GetBy(1)).Returns(context.HogentGroup);
+            GroupRepo.Setup(g => g.GetBy(2)).Returns(context.HogentGroupSubmitted);
+            GroupRepo.Setup(g => g.GetBy(3)).Returns(context.HogentGroupApproved);
+            GroupRepo.Setup(g => g.GetBy(4)).Returns(context.HogentGroupGranted);
+            GroupRepo.Setup(g => g.GetBy(5)).Returns(context.HogentGroupAnnounced);
+
+            _controller = new ActionController(PartRepo.Object, GroupRepo.Object);
+            _controller.TempData = new Mock<ITempDataDictionary>().Object;
+
+            _OwnerGranted = context.OwnerHogentGranted.UserState as Participant;
+            _OwnerAnnounced = context.OwnerHogentAnnounced.UserState as Participant;
         }
+        #endregion
+
+        #region Create
+
+        [Fact]
+        public void ParticipantCanCreateNewAction() {
+            ViewResult result = _controller.Create(_OwnerAnnounced, 5) as ViewResult;
+            CreateViewModel vm = (CreateViewModel)result?.Model;
+            Assert.Equal(_OwnerAnnounced.Group, vm.Group);
+        }
+
         #endregion
     }
 }
