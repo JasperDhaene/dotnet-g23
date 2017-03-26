c4ef3da6c20d2038fa985f37eb5bfe5feead7258
PrebenLeroy <preben.leroy@live.be>
2017-03-26 14:45:55 +0200

    start implementation TEst

--- dotnet-g23.Tests/Controllers/ActionControllerTest.cs
+++ dotnet-g23.Tests/Controllers/ActionControllerTest.cs
@@ -1,5 +1,8 @@
 ﻿using dotnet_g23.Controllers;
 using dotnet_g23.Models.Domain;
+using dotnet_g23.Models.Domain.Repositories;
+using dotnet_g23.Tests.Data;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +12,19 @@ namespace dotnet_g23.Tests.Controllers
 {
     public class ActionControllerTest
     {
+        #region Attributes
         private readonly ActionController _controller;
         private Participant _OwnerGranted;
         private Participant _OwnerAnnounced;
+        private DummyApplicationDbContext context;
+        #endregion
+
+        #region Controller
+        public ActionControllerTest() {
+            context = new DummyApplicationDbContext();
+
+            Mock<IParticipantRepository> PartRepo = new Mock<IParticipantRepository>();
+        }
+        #endregion
     }
 }
