6e0527946ac11f8875b61806a11feec04679e8bc
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-03-26 22:04:01 +0200

    Context testing 100% coverage

--- dotnet-g23.Tests/Models/State/ContextTest.cs
+++ dotnet-g23.Tests/Models/State/ContextTest.cs
@@ -2,6 +2,7 @@
 using dotnet_g23.Models.Domain.State;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
@@ -10,10 +11,35 @@ namespace dotnet_g23.Tests.Models.State
 {
     public class ContextTest
     {
+        private Context Context { get; set; }
+
+        public ContextTest()
+        {
+            Context = new Context();
+        }
+
         [Fact]
         public void ConstructorShouldCreateContextWithCurrentStateAsInitialState() {
-            Context context = new Context();
-            Assert.True(context.CurrentState is InitialState);
+            Assert.True(Context.CurrentState is InitialState);
+        }
+
+        [Fact]
+        public void SerializableStateShouldReturnString()
+        {
+            Assert.NotNull(Context.SerializableState);
+            Assert.NotEmpty(Context.SerializableState);
+        }
+
+        [Fact]
+        public void ContextShouldSerializeStateCorrectly() {
+            Context context = new Context { CurrentState = new ApprovedState() };
+            Assert.Contains("dotnet_g23.Models.Domain.State.ApprovedState", context.SerializableState);
+        }
+
+        [Fact]
+        public void ContextShouldDeSerializeStateCorrectly() {
+            Context context = new Context {SerializableState = "dotnet_g23.Models.Domain.State.AnnouncedState" };
+            Assert.True(context.CurrentState is AnnouncedState);
         }
     }
 }
